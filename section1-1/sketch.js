0cf0ef1db0b04e00311c4a3e9d206ede99734754
Nanako Kizaki <1990611h@stu.kobe-u.ac.jp>
2020-10-13 11:50:25 +0900

    Update sketch.js

--- section1-1/sketch.js
+++ section1-1/sketch.js
@@ -4,8 +4,6 @@ function setup() {
 
 function draw() {
 
-
-
   background(0);
   strokeWeight(4);
   fill(178, 100, 162);
@@ -13,7 +11,7 @@ function draw() {
   fill(94, 185, 84);
   triangle(0,108,106,108,106,0);
   fill(255);
-  textSize(32);
+  textSize(25);
   textFont("serif");
   text("46", 68, 100);
 }
