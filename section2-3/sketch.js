79ba559c8a3590f58471313b2f0064fb58b0d5bd
Nanako Kizaki <1990611h@stu.kobe-u.ac.jp>
2020-10-27 10:48:00 +0900

    HW
    宿題です

--- section2-3/sketch.js
+++ section2-3/sketch.js
@@ -5,9 +5,9 @@ function setup(){
   createCanvas(100, 100);
   for(let i = 0; i < 10; i++){
     for(let j = 0; j < 10; j++){
-      fill(j % 2 == 0 ? 255 : 0); // ここを変えます
+      fill(i % 2 == 1 ? 255 : 0); // ここを変えます
       console.log(i, j);
-      rect(i * 10, j * 10, 5, 5);
+      rect(i * 20, j * 20, 10, 10);
     }
   }
 }
