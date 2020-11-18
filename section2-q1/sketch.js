7b3cc381babb5716fb114897289caf6caced3601
Nanako Kizaki <1990611h@stu.kobe-u.ac.jp>
2020-11-18 16:26:52 +0900

    section2q-1
    小手先修正しました。アドバイスありがとうございました。

--- section2-q1/sketch.js
+++ section2-q1/sketch.js
@@ -3,15 +3,20 @@ function setup(){
   createCanvas(100,100);
 
 noFill();
-  for(let i = 0; i < 5; i++){
+  for(let i = 0; i<5;i++){
+
+  //i < 5; i++){
 
     ellipse(50,50,i*10);
 
 stroke(0, 0, 255);
   }
-for(let j = 5; j<10; j++){
+for(let j = 4; j<10; j++){
   ellipse(50,50,j*10)
 
 stroke(255, 0, 0);
 }
 }
+
+
+//j=5
