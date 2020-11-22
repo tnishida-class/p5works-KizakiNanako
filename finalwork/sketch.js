7c9091e609a6ef89b21ad6e6923233d52bd39595
Nanako Kizaki <1990611h@stu.kobe-u.ac.jp>
2020-11-22 17:33:52 +0900

    Update sketch.js

--- finalwork/sketch.js
+++ finalwork/sketch.js
@@ -1,11 +1,28 @@
 // 最終課題を制作しよう
 let x,y,r;
 let animate;
+
 function setup(){
   createCanvas(windowWidth, windowHeight);
   animate=true;
+  balloon("provisional movement",255,255,255,0,0,255,60);
 }
+
+function balloon(t,a,b,c,i,u,o,y){
+let w= textWidth
+let h= textAscent
+textDescent();
+let p=2
+fill(i,u,o);
+rect(0,0,w+p*2,h+p*2);
+fill(a,b,c);
+text(t,p,h+p);
+}
+
   function draw(){
+
+
+
 y =0.5*height;
 x =0.5*width;
 r=200;
