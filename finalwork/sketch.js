b9295e46393432aaa5c6abcd0dc9cd7b0d7aeefc
Nanako Kizaki <1990611h@stu.kobe-u.ac.jp>
2020-11-23 18:11:24 +0900

    finalwork

--- finalwork/sketch.js
+++ finalwork/sketch.js
@@ -5,42 +5,54 @@ let animate;
 function setup(){
   createCanvas(windowWidth, windowHeight);
   animate=true;
-  balloon("provisional movement",255,255,255,0,0,255,60);
+
 }
 
-function balloon(t,a,b,c,i,u,o,y){
-let w= textWidth
-let h= textAscent
-textDescent();
-let p=2
-fill(i,u,o);
-rect(0,0,w+p*2,h+p*2);
-fill(a,b,c);
-text(t,p,h+p);
-}
+
 
   function draw(){
+    background(160, 192, 255);
+    balloon("apparent movement",255,255,255,255,0,255,60);
+
+  function balloon(z,a,b,c,i,u,o){
 
 
 
-y =0.5*height;
-x =0.5*width;
-r=200;
-let t = millis() % 2000;
-  background(160, 192, 255);
 
-  if(t>1000&&animate){
-    line(x,y,x,y-r);
-  }
-    else{
-      line(x,y,x-r,y);
-  }
-  }
+    let w= textWidth();
+    let h= textAscent();
+    textDescent();
+    let p=0.1*width;
+  fill(i,u,o);
+    rect(0,0,w+p*2,h+p*2);
+    fill(a,b,c);
+    text(z,p,h+p);
+    }
 
-function windowResized(){
-  resizeCanvas(windowWidth, windowHeight);
-}
+    y =0.5*height;
+    x =0.5*width;
+    r=200;
+    let t = millis() % 2000;
 
-function mouseClicked(){
-  animate=!animate
-}
+
+      if(t>1000&&animate){
+        line(x,y,x,y-r);
+
+      }
+        else{
+          line(x,y,x-r,y);
+      }
+
+
+
+    }
+
+
+
+    function windowResized(){
+      resizeCanvas(windowWidth, windowHeight);
+    }
+
+    function mouseClicked(){
+      animate=!animate
+    }
